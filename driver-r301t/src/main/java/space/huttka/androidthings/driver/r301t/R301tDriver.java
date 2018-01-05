d06828d53f194e1efe74a8320fc01e3080ab17a7
Leonid Meleshin <leon.03.99@gmail.com>
2018-01-06 00:58:55 +0300

    Initial Setup

---
+++ driver-r301t/src/main/java/space/huttka/androidthings/driver/r301t/R301tDriver.java
@@ -0,0 +1,19 @@
+package space.huttka.androidthings.driver.r301t;
+
+import android.content.Context;
+
+/**
+ * @author leon0399
+ */
+
+public class R301tDriver implements AutoCloseable {
+
+    public R301tDriver(Context context, String TxUartName, String RxUartName) {
+
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
